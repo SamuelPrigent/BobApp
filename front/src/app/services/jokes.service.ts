2b0b75cc03465543135c54c250f55c80cf1eebd9
Samuel Prigent <samuel.prigent@yahoo.fr>
2025-06-20 13:07:37 +0200

    test CI/DI

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -4,10 +4,9 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
-
   private pathService = 'api/joke';
 
   private subject: BehaviorSubject<Joke | null> = new BehaviorSubject<Joke | null>(null);
@@ -20,7 +19,7 @@ export class JokesService {
     this.httpClient.get<Joke>(this.pathService).subscribe((joke: Joke) => this.subject.next(joke));
   }
 
-  public joke$(): Observable<Joke | null > {
+  public joke$(): Observable<Joke | null> {
     return this.subject.asObservable();
   }
 }
