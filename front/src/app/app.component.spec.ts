73863b38d840cd6d69739f76d87d872b513ea0b1
Samuel Prigent <samuel.prigent@yahoo.fr>
2025-06-20 16:57:06 +0200

    without modificaiton spec.ts

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -2,15 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { JokesService } from './services/jokes.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDividerModule } from '@angular/material/divider';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule, MatToolbarModule, MatCardModule, MatDividerModule],
+      imports: [HttpClientTestingModule],
       providers: [JokesService],
     }).compileComponents();
   });
